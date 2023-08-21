60b310ff303b88066e72ed31f0718baa45410921
sebastien <sebastien.maillet@gmail.com>
2023-08-21 09:28:10 +0200

    projet glacier premier commit

---
+++ role.sql
@@ -0,0 +1,5 @@
+insert into role (id, nom) values (1, 'role_1');
+insert into role (id, nom) values (2, 'role_2');
+insert into role (id, nom) values (3, 'role_3');
+insert into role (id, nom) values (4, 'role_4');
+insert into role (id, nom) values (5, 'role_5');
